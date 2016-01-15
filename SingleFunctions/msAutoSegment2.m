36b81241c236605921ae5c3d0abf8614c0f4dee0
daharoni <dbaharoni@gmail.com>
2016-01-14 22:22:36 -0800

    Added comments and a few corrections

--- SingleFunctions/msAutoSegment2.m
+++ SingleFunctions/msAutoSegment2.m
@@ -1,68 +1,81 @@
 function vidObj = msAutoSegment2(vidObj, frameLimits, cellAreaLimits, downSamp,corrCoefThreshDefault)
-%MSAUTOSEGMENT Summary of this function goes here
-%   Detailed explanation goes here
+%MSAUTOSEGMENT2 An serial iterative algorithm for identifying the boundries
+%of active neurons.
+%   vidObj = The Miniscope data structure which contains location of video
+%   files, bright spot information, and preprocessing corrections
+%   frameLimits= Should just pass in an empty array '[]'. This variable
+%   doesn't do anything anymore
+%   cellAreaLimits = [minArea maxArea]. min and max pixel area for a region
+%   to be considered a possible neuron
+%   downSamp = down sample factor. This algorithm loads all video data into
+%   RAM. Make sure the waveform shape of dF/F activity is not lost. We down
+%   sample by a factor of 5 when imaging GCaMP6F at 30FPS.
+%   corrCoefThreshDefault = Minimum threshold for surrounding pixels to be
+%   grouped in to the ROI on each iteration
 
-cVideo = 0;
+
 if isempty(frameLimits)
     frameLimits = [1 vidObj.numFrames];
 end
-
-% Added in for making video
-%--------------------------
     
-count = 0;
-%these settings can be changed.
-% brightnessThreshDefault = 0.85;%0.9;
-% corrCoefThreshDefault = 0.9;%0.86;
-
 %smoothing kernal
 hSmall = fspecial('average', 5);
 hLarge = fspecial('average', 60);
 
+%Used for plotting (Generally plotting should be commented out)
 green = cat(3, zeros(vidObj.alignedHeight,vidObj.alignedWidth), ...
     ones(vidObj.alignedHeight,vidObj.alignedWidth), ...
     zeros(vidObj.alignedHeight,vidObj.alignedWidth));
 green(:,:,1) = 10*vidObj.brightSpots/max(vidObj.brightSpots(:));
 ttt = green(:,:,1)==0;
 green(:,:,2) = ttt;
+%--------------------------------------------------------------------------
 
 %corrWindow holds the the data/info of the current section of video
 %being analyzed
-window.size =  20;
-time.size = 20;
-seg.initialSize = 2;
+
+%The values below are the length of half of the side of a square.
+window.size =  20; %The window size to run pixel correlations on
+time.size = 20; %The size of the window of bright spot timings to use
+seg.initialSize = 2; %initial size of pixel box to include in pixel group G_px
+%--------------------------------------------------------------------------
 
 % kernal used for eroding and dilating
 se = strel('diamond',10);
 se2 = strel('diamond',4);
+
 %location where segments are stored
 if ~isfield(vidObj,'segments')
     vidObj.segments = [];
     vidObj.segMat = zeros(vidObj.alignedHeight,vidObj.alignedWidth);
     vidObj.segementOutline = zeros(vidObj.alignedHeight,vidObj.alignedWidth);
 end
-%allocation of memory
 
+%allocation of memory
 numFramesDown = length(1:downSamp:vidObj.numFrames);
 data = uint8(nan(vidObj.alignedHeight,vidObj.alignedWidth,numFramesDown));
 count = 0;
 brightSpots = vidObj.brightSpots;
 brightSpots = filter2(ones(3,3),brightSpots);
 initialNumSpots = sum(brightSpots(:));
-if ~isempty(vidObj.segments)
+
+if ~isempty(vidObj.segments) %if vidObjs already has segments present
     brightSpots(sum(vidObj.segments,3)>0) = 0;
 end
+
+%Loads down sampled video into memory and calculates dF/F max projection
 for frameNum=1:downSamp:vidObj.numFrames
     count = count +1;
      if (mod(frameNum,1+100*downSamp)==0)
             display(['Reading video into memory. ' num2str(frameNum/vidObj.numFrames*100) '% done'])
      end
-    data(:,:,count) = msReadFrame(vidObj,frameNum,true,true,false);
+    data(:,:,count) = msReadFrame(vidObj,frameNum,true,true,false); %loads dF/F frame
 end
 frameMax = squeeze(max(data,[],3));
 data = reshape(data,[],numFramesDown);
+%--------------------------------------------------------------------------
 
-% lowpass filter pixel dF/F
+% lowpass filter pixel dF/F to remove some noise
 [bFilt,aFilt] = butter(2,  1/((30/downSamp)/2), 'low');
 for i=1:vidObj.alignedHeight
     tempData = double(data((1:vidObj.alignedWidth)+vidObj.alignedWidth*(i-1),:)');
@@ -71,47 +84,49 @@ for i=1:vidObj.alignedHeight
 end
 %--------------------------
 
+
 [numCounts,index] = max(brightSpots(:));
 f = figure(1);
-while (sum(brightSpots(:)) > 1) && (numCounts>=5)
 
+%While loop will run through the pixel location with the highest number of
+%bright spots detected until there are only pixels with counts less than brightSpotsStopAmount
+brightSpotsStopAmount = 5; %Counts few than 5 are generally due to noise and slow down the segmentation algorithm
+while (sum(brightSpots(:)) > 1) && (numCounts>=brightSpotsStopAmount)
+    
+    %centroid is the pixel location with the current highest count of
+    %bright spots
     centroid(1) = mod(index,vidObj.alignedHeight);
     centroid(2) = floor(index/vidObj.alignedHeight)+1;
     
+    %window holds the information for the iterative correlation algorithm
+    %below. Used to select only the near by pixels in 'data' to run correlation on
     window.mask = zeros(vidObj.alignedHeight,vidObj.alignedWidth);
     window.width = uint16(max([1 centroid(2)-window.size]):min([vidObj.alignedWidth centroid(2)+window.size]));
     window.height = uint16(max([1 centroid(1)-window.size]):min([vidObj.alignedHeight centroid(1)+window.size]));
         
-    subplot_tight(4,4,[1 2 3 5 6 7 9 10 11],[1 1]*.05);
-
-%     h =imshow((frameMax),[50 150]);
-pcolor(double(frameMax))
-    colormap gray;
-    daspect([1 1 1])
-    shading flat
-    freezeColors
-% drawnow
-%     hold on
+    % Plotting stuff. Uncomment if you want to see what is going on
     
-%     h = imshow(vidObj.brightSpots,[0 10]);
-%     drawnow
-%     set(h,'AlphaData',vidObj.brightSpots~=0);
-%     drawnow
+%     subplot_tight(4,4,[1 2 3 5 6 7 9 10 11],[1 1]*.05);
+% 
+% pcolor(double(frameMax))
+%     colormap gray;
 %     daspect([1 1 1])
 %     shading flat
-    hold on
-    hhh = imshow(green);
+%     freezeColors
+% 
+%     hold on
+%     hhh = imshow(green);
+%     set(hhh,'AlphaData',(vidObj.segementOutline)|(vidObj.brightSpots>=2));
+%     rectangle('Position',[centroid(2)-window.size,centroid(1)-window.size,window.size*2,window.size*2]);
+%     hold off
 %     drawnow
-    set(hhh,'AlphaData',(vidObj.segementOutline)|(vidObj.brightSpots>=2));
-%     drawnow
-%     plot(centroid(2),centroid(1),'ro','markersize',30)
-    rectangle('Position',[centroid(2)-window.size,centroid(1)-window.size,window.size*2,window.size*2]);
-    hold off
-    drawnow
-    if ~isempty(vidObj.segments)
-        title(['Current Count: ' num2str(numCounts) ' | Counts left: ' num2str(sum(brightSpots(:))/sum(initialNumSpots)*100) '% | Number of Segments: ' num2str(size(vidObj.segments,3))])
-%     title(['Number of Segments: ' num2str(size(vidObj.segments,3))])
-    end
+    
+%     if ~isempty(vidObj.segments)
+%         title(['Current Count: ' num2str(numCounts) ' | Counts left: ' num2str(sum(brightSpots(:))/sum(initialNumSpots)*100) '% | Number of Segments: ' num2str(size(vidObj.segments,3))])
+%     end
+
+    %center of window. if statements handle if the window is overlapping
+    %the edge of the frame
     if (window.width(1) == 1)
         window.centerMaskW = length(window.width) - window.size;
     else
@@ -122,58 +137,85 @@ pcolor(double(frameMax))
     else
         window.centerMaskH = window.size;
     end
+    %----------------------------------------------------------------------
+    
+    %window.mask and window.maskVect are masks containing 1's for pixels to run correlation on
     window.mask(window.height, window.width) = 1;
     window.maskVect = window.mask(:);
     
+    %'time' is used to select the relevant frames for the following
+    %correlation. We want to run correlation only around frames where there
+    %was activity (i.e. dF/F bright spots).
     time.mask = zeros(vidObj.alignedHeight,vidObj.alignedWidth);
     time.width = uint16(max([1 centroid(2)-time.size]):min([vidObj.alignedWidth centroid(2)+time.size]));
     time.height = uint16(max([1 centroid(1)-time.size]):min([vidObj.alignedHeight centroid(1)+time.size]));
     time.mask(time.height, time.width) = 1;
     time.maskVect = time.mask(:);
+    %----------------------------------------------------------------------
     
+    %timesInWindow holds all the frame numbers of when bright spots occured
+    %in time.mask
     timeWindow = zeros(1,numFramesDown);
     timesInWindow = vidObj.brightSpotTiming(time.maskVect==1,:);
     timesInWindow = timesInWindow(:);
     timesInWindow = timesInWindow(timesInWindow~=0);
+    %----------------------------------------------------------------------
+    
     for i=1:length(timesInWindow) %% need to add framerate and window width adjustments
         temp = timesInWindow(i)/downSamp;
         timeWindow(max([1 round(temp-30/downSamp)]):min([numFramesDown round(temp+150/downSamp)])) = 1; %number are time window size around event
-%         [i sum(timeWindow)]
     end
-%     waitforbuttonpress
+    
+    %seg.mask holds the ROI of the current segment being detected
     seg.mask = zeros(vidObj.alignedHeight,vidObj.alignedWidth);
     seg.width = uint16(max([1 centroid(2)-seg.initialSize]):min([vidObj.alignedWidth centroid(2)+seg.initialSize]));
     seg.height = uint16(max([1 centroid(1)-seg.initialSize]):min([vidObj.alignedHeight centroid(1)+seg.initialSize]));
-
+    
     seg.mask(seg.height,seg.width) = 1;
     seg.maskVect = seg.mask(:);
-%     pcolor(seg.mask)
-%     shading flat
-    kill = 0;
-    attempts = 0;
-    previousMaskSum = 0;
-    corrCoefThresh = corrCoefThreshDefault;
-%     brightnessThresh = brightnessThreshDefault;
+    %----------------------------------------------------------------------
     
+    kill = 0; %used in case interative algorithm get stuck
+    attempts = 0;
+    previousMaskSum = 0; %prevoious size of ROI
+    corrCoefThresh = corrCoefThreshDefault;
+    
+    %Used to approx. dF/F. F ~= meanFrame
     meanFrameWindow = vidObj.meanFrame{vidObj.selectedAlignment}(window.maskVect==1);
     meanFrameWindow = repmat(meanFrameWindow,1,sum(timeWindow));
+    %----------------------------------------------------------------------
+    
+    %Start of iterative algorithm
     while (kill == 0)
         attempts = attempts +1;
+        
+        %Calculate mean dF/F of all pixels currently in ROI
         seg.mean = mean(double(data(seg.maskVect==1,timeWindow==1))./repmat(vidObj.meanFrame{vidObj.selectedAlignment}(seg.maskVect==1),1,sum(timeWindow)),1);       
+        %Calculate approx individual dF/F of surrounding pixels
         temp3 = double(data(window.maskVect==1,timeWindow==1))./meanFrameWindow;
+        %------------------------------------------------------------------
         
         %dF/dt
 %         seg.mean = diff(seg.mean);
 %         temp3 = diff(temp3,1,2);
         %-----
+        
+        %Calculate correlation of current segment dF/F with individual
+        %surrounding pixels
         xCorrCoef2 = corr(double(temp3'),seg.mean');
         xCorrCoef = reshape(xCorrCoef2,length(window.height),length(window.width));
-
+        %------------------------------------------------------------------
+        
+        %Surrounding pixels with high correlation coefficients get grouped
+        %into the segment
         seg.mask(window.height,window.width) = ((seg.mask(window.height,window.width) == 1) | (xCorrCoef > corrCoefThresh));
         
         seg.mask = imdilate(seg.mask,se);
         seg.mask = imerode(seg.mask,se);
-    
+        %------------------------------------------------------------------
+        
+        %Making sure the pixel group we are keeping is the one containing
+        %the initial group
         regionStats = regionprops(logical(seg.mask), 'Area', 'BoundingBox', 'Image', 'PixelIdxList');
         c = 0;
         maskSum = 0;
@@ -192,68 +234,54 @@ pcolor(double(frameMax))
             attempts = inf;
             display('Segment moved too far away');
         end
+        %------------------------------------------------------------------
         
-        cVideo = cVideo+1;
-        filename = ['video/frame' num2str(cVideo) '.jpg'];
+        % Plotting stuff. Uncomment if you want to see what is going on.
         
-        subplot_tight(4,4,[4],[1 1]*.05);                
-        pcolor(xCorrCoef);
-        colormap jet
-        title('CorrCoef')
-        shading flat
-        caxis([0 1])
-        daspect([1 1 1])
-        set(gca,'xTick',[])
-        set(gca,'yTick',[])
-        freezeColors
-%         subplot_tight(4,4,[8],[1 1]*.05);  
-%         pcolor((xCorrCoef > corrCoefThresh)*1);
+%         subplot_tight(4,4,[4],[1 1]*.05);                
+%         pcolor(xCorrCoef);
 %         colormap jet
+%         title('CorrCoef')
+%         shading flat
+%         caxis([0 1])
+%         daspect([1 1 1])
+%         set(gca,'xTick',[])
+%         set(gca,'yTick',[])
 %         freezeColors
-%         title(['CorrCoef Mask'])
+% 
+%         subplot_tight(4,4,12,[1 1]*.05);  
+%         pcolor(seg.mask(window.height,window.width)*1)
+%         colormap jet
+%         set(gca,'xTick',[])
+%         set(gca,'yTick',[])
+%         freezeColors
+%         title(['Final Mask | NumPixels:' num2str(maskSum)])
 %         shading flat
 %         daspect([1 1 1])
-%                 subplot_tight(4,4,[12],[1 1]*.05);  
-%                 pcolor(brightnessMask*1);
-%                 colormap jet
-%                 freezeColors
-%                 title(['Brightness Mask | Thresh:' num2str(brightnessThresh)])
-%                 shading flat
-%                 daspect([1 1 1])
-        subplot_tight(4,4,12,[1 1]*.05);  
-        pcolor(seg.mask(window.height,window.width)*1)
-        colormap jet
-        set(gca,'xTick',[])
-        set(gca,'yTick',[])
-        freezeColors
-        title(['Final Mask | NumPixels:' num2str(maskSum)])
-        shading flat
-        daspect([1 1 1])
-        subplot_tight(4,4,[13 14 15 16],[1 1]*.05);  
-                ttt = find(xCorrCoef2<corrCoefThresh);
-%                 length(ttt)
-                ttt = ttt(ceil(length(ttt)*rand(ceil(length(ttt)/100),1)));
-                if (~isempty(ttt))
-                    plot(((1:size(temp3,2))*downSamp/30)',temp3(ttt,:)'-1,'r');
-                    hold on 
-                end
-                ttt = find(xCorrCoef2>=corrCoefThresh);
-                ttt = ttt(ceil(length(ttt)*rand(ceil(length(ttt)/30),1)));
-                if (~isempty(ttt))
-                    plot(((1:size(temp3,2))*downSamp/30)',temp3(ttt,:)'-1,'g');
-                end
-                plot(((1:size(temp3,2))*downSamp/30)',seg.mean-1,'k','linewidth',2);
-                hold off
-%                 axis([-corrWindow.frameLength corrWindow.frameLength -.1 .3])
-                title('Pixel intensity')
-                xlabel('time (s)');
-                ylabel('dF/F');
-                ylim([-0.1 .5])
-                xlim([0 size(temp3,2)*downSamp/30])
-        drawnow
-        % Added in for making video
-%             saveas(f,filename);
-        %----------------------------------
+%         subplot_tight(4,4,[13 14 15 16],[1 1]*.05);  
+%                 ttt = find(xCorrCoef2<corrCoefThresh);
+% %                 length(ttt)
+%                 ttt = ttt(ceil(length(ttt)*rand(ceil(length(ttt)/100),1)));
+%                 if (~isempty(ttt))
+%                     plot(((1:size(temp3,2))*downSamp/30)',temp3(ttt,:)'-1,'r');
+%                     hold on 
+%                 end
+%                 ttt = find(xCorrCoef2>=corrCoefThresh);
+%                 ttt = ttt(ceil(length(ttt)*rand(ceil(length(ttt)/30),1)));
+%                 if (~isempty(ttt))
+%                     plot(((1:size(temp3,2))*downSamp/30)',temp3(ttt,:)'-1,'g');
+%                 end
+%                 plot(((1:size(temp3,2))*downSamp/30)',seg.mean-1,'k','linewidth',2);
+%                 hold off
+% %                 axis([-corrWindow.frameLength corrWindow.frameLength -.1 .3])
+%                 title('Pixel intensity')
+%                 xlabel('time (s)');
+%                 ylabel('dF/F');
+%                 ylim([-0.1 .5])
+%                 xlim([0 size(temp3,2)*downSamp/30])
+%         drawnow
+
+    % Checks to see if segment stopped growing or too many iterations have passed
         if (maskSum > cellAreaLimits(2))
             attempts = inf;
             kill = 1;
@@ -274,7 +302,7 @@ pcolor(double(frameMax))
         end
         
     end
-    
+    % Adds final segment ROI to vidObj.segment
     brightSpots(seg.maskVect == 1) = 0;
     seg.mask = imdilate(seg.mask,se);
     seg.mask = imerode(seg.mask,se);
@@ -292,21 +320,9 @@ pcolor(double(frameMax))
     [numCounts,index] = max(brightSpots(:));
 end
                
-
-%     frameNum = frameNum + stepSize;
-%     corrWindow.frame(:,:,1:end-stepSize) = corrWindow.frame(:,:,(stepSize+1):end);
-%     corrWindow.frame(1,1,(end-stepSize):end) = nan;
-%     curserW = [];
-%     
-    
-    
-% end
 vidObj.segments = uint8(vidObj.segments); %added 10_8_2014 to decrease memory size
 vidObj.cellAreaLimits = cellAreaLimits;
 vidObj.numSegments = size(vidObj.segments,3);
 
-%Added for making video
-
-%----------------------
 end
 
--- SingleFunctions/msFindBrightSpots.m
+++ SingleFunctions/msFindBrightSpots.m
@@ -1,13 +1,23 @@
 function vidObj = msFindBrightSpots(vidObj,stepSize, frameLimits,dFFTresh, backgroundTresh)
-%MSFINDBRIGHTSPOTS Summary of this function goes here
-%   Detailed explanation goes here
+%MSFINDBRIGHTSPOTS Function will identify the frame and pixel location of
+%local maxima of dF/F across entire video. msFindBrightSpots will step
+%through the video detecting locations that exceed dFFThresh and
+%backgroundThresh.  This information is then used in msAutoSegment2
+%   vidObj = The Miniscope data structure which contains location of video
+%   files and preprocessing corrections
+%   stepSize = number of frames to step by
+%   frameLimits = [startFrameNumber endFrameNumber]. Leave this as an empty
+%   array '[]' if you want to run through the entire video
+%   dFFTresh = Local maxima need to be above this threshold to be detected.
+%   Usually 0.03 to 0.1 works well
+%   backgroundTresh = how much brighter a local maxima needs to be over the
+%   surrounding background to be detected. 0.02 to 0.05 seem to work well.
 
 
-if isempty(frameLimits)
+if isempty(frameLimits) 
     frameLimits = [1 vidObj.numFrames];
 end
 
-count = 0;
 
 %smoothing kernal
 hSmall = fspecial('average', 3);
@@ -17,6 +27,7 @@ red = cat(3, ones(vidObj.alignedHeight,vidObj.alignedWidth), ...
     zeros(vidObj.alignedHeight,vidObj.alignedWidth), ...
     zeros(vidObj.alignedHeight,vidObj.alignedWidth));
 
+%used for overlay in display
 green = cat(3, zeros(vidObj.alignedHeight,vidObj.alignedWidth), ...
     ones(vidObj.alignedHeight,vidObj.alignedWidth), ...
     zeros(vidObj.alignedHeight,vidObj.alignedWidth));
@@ -24,31 +35,39 @@ green = cat(3, zeros(vidObj.alignedHeight,vidObj.alignedWidth), ...
 % kernal used for eroding and dilating
 se = strel('diamond',10);
 se2 = strel('diamond',2);
-%location where segments are stored
 
 frame = nan(vidObj.alignedHeight,vidObj.alignedWidth,stepSize);
-vidObj.brightSpots = zeros(vidObj.alignedHeight,vidObj.alignedWidth);
-vidObj.brightSpotTiming = sparse(vidObj.alignedHeight*vidObj.alignedWidth,0);
+vidObj.brightSpots = zeros(vidObj.alignedHeight,vidObj.alignedWidth); %each cell (or pixel) contains the count of local maxima detected
+vidObj.brightSpotTiming = sparse(vidObj.alignedHeight*vidObj.alignedWidth,0); %holds the spatiotemporal information of detected bright spots
 
-for startFrameNum=frameLimits(1):stepSize:min([frameLimits(2) vidObj.numFrames-stepSize])
+for startFrameNum=frameLimits(1):stepSize:min([frameLimits(2) vidObj.numFrames-stepSize]) %steps through data video
    
-    %loads needed frames centered around the current frame being
-    %displayed
     count = 0;
-    for frameNum = startFrameNum:(startFrameNum+stepSize)
+    % Generates a max projection of the frames contained in the current
+    % step. Applies a small spatial filter to the max projection to remove noise.
+    % Applies a large spatial filter to get a measure of the background
+    % activity.
+    for frameNum = startFrameNum:(startFrameNum+stepSize) 
         count = count+1;
         frame(:,:,count) =filter2(hSmall,msReadFrame(vidObj,frameNum,true,true,true));
     end
     frameMax = max(frame,[],3);
     frameBase = filter2(hLarge,frameMax);
+    %----------------------------------------------------------------------
+    
     f = figure(1);
     clf
     hold off
+    %Applies dFFThreshold and backgroundThreshold to the max projection.
+    %Erodes the resulting mask to remove noise
     bw = zeros(size(frameMax));
     bw((frameMax-frameBase >= backgroundTresh) & (frameMax >dFFTresh)) = 1;
     bw = imerode(bw,se2);
     %         bw = imdilate(bw,se2);
+    %----------------------------------------------------------------------
     
+    %Finds the centroid of regions in the thresholded max projection
+    %Records their pixel location and frame number
     bwProps = regionprops(logical(bw), frameMax, 'Area', 'BoundingBox', 'Image', 'PixelIdxList','Centroid','WeightedCentroid');
     for propNum = 1:length(bwProps)
         centroid = floor(bwProps(propNum).WeightedCentroid);        
@@ -57,7 +76,10 @@ for startFrameNum=frameLimits(1):stepSize:min([frameLimits(2) vidObj.numFrames-s
             vidObj.brightSpotTiming(centroid(2)+(centroid(1)-1)*vidObj.alignedHeight,...
                 vidObj.brightSpots(centroid(2),centroid(1))) = frameNum;
         end
-    end      
+    end  
+    %----------------------------------------------------------------------
+    
+    %Plotting (Can be commented out)
     subplot_tight(1,2,1,0.05*[1 1])
     pcolor(frameMax)
     caxis([-0.05 .3]) %sets visial dF/F range    
@@ -75,5 +97,7 @@ for startFrameNum=frameLimits(1):stepSize:min([frameLimits(2) vidObj.numFrames-s
     shading flat
     colormap jet
     freezeColors
+    %----------------------------------------------------------------------
+
 end
 
